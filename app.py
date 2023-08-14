dcb135293a7f8de8d7567947348881c44718da83
Adanene <128061161+Adanene@users.noreply.github.com>
2023-08-14 10:36:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ print('Mean squared error:', mse)
 
 # Predict stability for a new inclining test
 #make the interface
-st.title("Ship inclining prediction Ver 0.033")
+st.title("Ship inclining prediction Ver 0.035")
 
 st.write("""### We need some data to predict ship inclining angle""")
 
