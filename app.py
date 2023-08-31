c614f32ebc1e0f5f7d038e969a4eeb6ca96d1f9b
Adanene <128061161+Adanene@users.noreply.github.com>
2023-08-31 09:33:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ data = fetch_data()
 
 # Predict stability for a new inclining test
 #make the interface
-st.title("Ship inclining prediction Ver 0.8")
+st.title("Ship inclining prediction Ver 0.81")
 
 st.write("""### We need some data to predict ship inclining angle""")
 
