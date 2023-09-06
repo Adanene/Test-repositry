244b0fb21c6af4553546301ce598b65895244b1c
Adanene <128061161+Adanene@users.noreply.github.com>
2023-09-06 19:43:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -93,11 +93,11 @@ if ok:
     
     # Define the parameter grid
     param_grid = {
-        'n_estimators': [600, 650, 700, 750, 800], 
-        'max_depth': [7, 8, 9, 10, 11],
-        'learning_rate': [0.05, 0.075, 0.1, 0.125],
-        'subsample': [0.6, 0.7, 0.8, 0.9, 1.0],
-        'colsample_bytree': [0.6, 0.7, 0.8 , 0.9, 1.0]
+        'n_estimators': [100, 250, 500, 750], 
+        'max_depth': [3, 5, 7, 9],
+        'learning_rate': [0.01, 0.05, 0.1],
+        'subsample': [0.8, 0.9, 1.0],
+        'colsample_bytree': [0.8 , 0.9, 1.0]
     }
 
     # Create the XGBoost regressor
