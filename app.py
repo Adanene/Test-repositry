16ae3fca7d047f03073adc961143ceaa6b51e819
Adanene <128061161+Adanene@users.noreply.github.com>
2023-09-09 22:14:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ if ok:
     param_grid = {
         'n_estimators': [400, 550, 800, 900], 
         'max_depth': [3, 4, 5, 6],
-        'learning_rate': [0.05, 0.06, 0.07],
+        'learning_rate': [0.045, 0.05, 0.055],
         'subsample': [0.8, 0.9, 1.0],
         'colsample_bytree': [0.8 , 0.9, 1.0]
     }
