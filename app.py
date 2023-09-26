53b824ea5c55e04041be8fc68182e3d7aaf37343
Adanene <128061161+Adanene@users.noreply.github.com>
2023-09-26 15:48:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ if ok:
     # chnge some data into numeric
 
     # Split the dataset into training and test sets
-    train_data, test_data = train_test_split(data, test_size=0.4, random_state=80)
+    train_data, test_data = train_test_split(data, test_size=0.3, random_state=80)
 
     # Select the features and target variable
     features = ['B/T', 'Cb', 'D/T', 'kirkan', 'beban/disp',]
