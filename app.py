f74affd6072d271a6b4470ef4ab6c2b8b48fba30
Adanene <128061161+Adanene@users.noreply.github.com>
2023-09-27 11:12:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -96,6 +96,246 @@ if ok:
     # chnge some data into numeric
 
     # Split the dataset into training and test sets
+    train_data, test_data = train_test_split(data, test_size=0.3, random_state=85)
+
+    # Select the features and target variable
+    features = ['B/T', 'Cb', 'D/T', 'Moment', 'beban/disp',]
+    target = 'Inclinement'
+    
+    # Define the parameter grid
+    param_grid = {
+        'n_estimators': [350], 
+        'max_depth': [9],
+        'learning_rate': [0.5],
+        'subsample': [1.0],
+        'colsample_bytree': [1.0]
+    }
+
+    # Create the XGBoost regressor
+    xgboost_model = xgb.XGBRegressor(random_state=600, objective="reg:squarederror")  # Note: objective is set to handle regression tasks
+
+    # Create the GridSearchCV object
+    grid_search = GridSearchCV(estimator=xgboost_model, param_grid=param_grid, 
+                           cv=3, n_jobs=-1, verbose=2, scoring='neg_mean_squared_error')
+
+    # Fit the GridSearchCV to the training data
+    grid_search.fit(train_data[features], train_data[target])
+
+    # Get the best model from GridSearchCV
+    best_model = grid_search.best_estimator_
+
+    # Make predictions on the test set
+    test_predictions = best_model.predict(test_data[features])
+
+    # Evaluate the model performance
+    mse = mean_squared_error(test_data[target], test_predictions)
+    print('Mean squared error:', mse)
+
+    # Note: XGBoost also provides feature importances similar to Random Forest
+    importances = best_model.feature_importances_
+    sorted_indices = np.argsort(importances)[::-1]
+
+if st.session_state.button_pressed:
+        if jumlah_beban =="0" :
+                st.subheader(f"the accuracy of this inclinement model is {mse}  " )
+        
+        else:
+                halfBreadth = Breadth/2
+                #transfer weight
+                beban_A = bebanA * halfBreadth
+                beban_B = bebanB * halfBreadth
+                beban_C = bebanC * halfBreadth
+                beban_D = bebanD * halfBreadth
+                beban_E = bebanE * halfBreadth
+                beban_F = bebanF * halfBreadth
+                totalB = (bebanA + bebanB + bebanC + bebanD + bebanE + bebanF)
+        
+                #calculate displacement
+                displacement = (Lwl * Breadth * Draft * Cb)
+
+                totdisp = (totalB / displacement)
+                
+
+                #calculate moment
+                if jumlah_beban == "4" :
+                # 4 pembebanan
+                        kanan1 = (beban_B + beban_D)
+                        kanan2 = (beban_D)
+                        kanan3 = 0
+                        kanan4 = (beban_A)
+                        kanan5 = (beban_A + beban_C)
+                        kanan6 = (beban_A + beban_B + beban_C)
+                        kanan7 = (beban_A + beban_B + beban_C + beban_D)
+                        kanan8 = (beban_B + beban_C + beban_D)
+
+                        kiri1 = (beban_A + beban_C)
+                        kiri2 = (beban_A + beban_B + beban_C)
+                        kiri3 = (beban_A + beban_B + beban_C + beban_D)
+                        kiri4 = (beban_B + beban_C + beban_D)
+                        kiri5 = (beban_B + beban_D)
+                        kiri6 = (beban_D)
+                        kiri7 = 0
+                        kiri8 = (beban_A)
+                        
+                        
+                
+                if jumlah_beban == "6" :
+
+                        kanan1 = (beban_A + beban_C + beban_E)
+                        kanan2 = (beban_A + beban_B + beban_C + beban_E)
+                        kanan3 = (beban_A + beban_B + beban_C + beban_D + beban_E + beban_F)
+                        kanan4 = (beban_A + beban_B + beban_C + beban_D + beban_E )
+                        kanan5 = (beban_A + beban_C + beban_E)
+                        kanan6 = (beban_E)
+                        kanan7 = 0
+                        kanan8 = (beban_C + beban_E)
+
+                        kiri1 = (beban_B + beban_D + beban_F)
+                        kiri2 = (beban_D + beban_F)
+                        kiri3 = 0
+                        kiri4 = (beban_F)
+                        kiri5 = (beban_B + beban_D + beban_F)
+                        kiri6 = (beban_A + beban_B + beban_C + beban_D + beban_F )
+                        kiri7 = (beban_A + beban_B + beban_C + beban_D + beban_E + beban_F)
+                        kiri8 = (beban_A + beban_B + beban_D + beban_F)
+                    
+                     
+                #finding ratio
+                if Breadth == 0:
+                        LB = 0
+                else :
+                        LB = (Lwl /Breadth) 
+
+                if Draft == 0:
+                        BT = 0
+                        DT = 0
+                else :
+                        BT = (Breadth /Draft) 
+                        DT = (Depth / Draft)
+
+                    
+                Mselisih1 =  (kiri1 - kanan1) 
+                Mselisih2 =  (kiri2 - kanan2)  
+                Mselisih3 =  (kiri3 - kanan3) 
+                Mselisih4 =  (kiri4 - kanan4) 
+                Mselisih5 =  (kiri5 - kanan5) 
+                Mselisih6 =  (kiri6 - kanan6) 
+                Mselisih7 =  (kiri7 - kanan7) 
+                Mselisih8 =  (kiri8 - kanan8)
+                
+                new_test1 = pd.DataFrame({ 'B/T' :[BT], 'Cb': [Cb], 'D/T' :[DT] , 'Moment': [Mselisih1], 'beban/disp' : [totdisp], })
+                predicted_Incline1 = best_model.predict(new_test1)
+        
+                new_test2 = pd.DataFrame({ 'B/T' :[BT], 'Cb': [Cb], 'D/T' :[DT] , 'Moment': [Mselisih2], 'beban/disp' : [totdisp], })
+                predicted_Incline2 = best_model.predict(new_test2)
+        
+                new_test3 = pd.DataFrame({ 'B/T' :[BT], 'Cb': [Cb], 'D/T' :[DT] , 'Moment': [Mselisih3], 'beban/disp' : [totdisp], })
+                predicted_Incline3 = best_model.predict(new_test3)
+        
+                new_test4 = pd.DataFrame({ 'B/T' :[BT], 'Cb': [Cb], 'D/T' :[DT] , 'Moment': [Mselisih4], 'beban/disp' : [totdisp], })
+                predicted_Incline4 = best_model.predict(new_test4)
+        
+                new_test5 = pd.DataFrame({ 'B/T' :[BT], 'Cb': [Cb], 'D/T' :[DT] , 'Moment': [Mselisih5], 'beban/disp' : [totdisp], })
+                predicted_Incline5 = best_model.predict(new_test5)
+        
+                new_test6 = pd.DataFrame({ 'B/T' :[BT], 'Cb': [Cb], 'D/T' :[DT] , 'Moment': [Mselisih6], 'beban/disp' : [totdisp], })
+                predicted_Incline6 = best_model.predict(new_test6)
+        
+                new_test7 = pd.DataFrame({ 'B/T' :[BT], 'Cb': [Cb], 'D/T' :[DT] , 'Moment': [Mselisih7], 'beban/disp' : [totdisp], })
+                predicted_Incline7 = best_model.predict(new_test7)
+        
+                new_test8 = pd.DataFrame({ 'B/T' :[BT], 'Cb': [Cb], 'D/T' :[DT] , 'Moment': [Mselisih8], 'beban/disp' : [totdisp], })
+                predicted_Incline8 = best_model.predict(new_test8)
+
+                st.subheader(f"the accuracy of this inclinement model is {mse} " )
+        
+                dataS = pd.DataFrame({
+                        'Moment Beban (Kg.m)': [Mselisih1, Mselisih2, Mselisih3, Mselisih4, Mselisih5, Mselisih6, Mselisih7, Mselisih8],
+                        'incline (degrees)': [predicted_Incline1[0], predicted_Incline2[0], predicted_Incline3[0], predicted_Incline4[0], predicted_Incline5[0], predicted_Incline6[0], predicted_Incline7[0], predicted_Incline8[0],
+                                   ]
+                        })
+                dataS_display = dataS.copy()
+                dataS_display['Moment Beban (Kg.m)'] = dataS['Moment Beban (Kg.m)']
+                dataS_display['incline (degrees)'] = dataS['incline (degrees)'].apply(lambda x: '{:.2f}'.format(x))
+
+                st.table(dataS_display)
+            
+               
+                # make graphics
+                fig, ax = plt.subplots()
+
+                # Create a scatter plot
+                scatter = ax.scatter(dataS['Moment Beban (Kg.m)'], dataS['incline (degrees)'], color='blue', label='Incliing result')
+        
+                # Set title, labels, and legend
+                ax.set_title("Inclining graphic")
+                ax.set_xlabel('Moment Beban (Kg.m)')
+                ax.set_ylabel('incline (degrees)')
+                ax.legend()
+
+                # Add annotations
+                for i in range(len(dataS)):
+                        ax.annotate(i, (dataS['Moment Beban (Kg.m)'].iloc[i], dataS['incline (degrees)'].iloc[i])) # i+1 because Python's indexing starts at 0
+
+                ax.set_xlabel('Moment Beban (Kg.m)')
+                ax.set_ylabel('incline (degrees)')
+
+                # Customization: draw a vertical line (you can adjust this as per your requirement)
+                threshold = dataS['Moment Beban (Kg.m)'].mean()  # example threshold using mean, adjust as needed
+                ax.axvline(x=0, color='red', linestyle='--', label=" 0,0 coordinate")
+                ax.legend()
+
+                # Customization: draw a horizontal  line (you can adjust this as per your requirement)
+                thresholds = dataS['incline (degrees)'].mean()  # example threshold using mean, adjust as needed
+                ax.axhline(y=0, color='red', linestyle='--', label="")
+                ax.legend()
+
+                # Display the plot in Streamlit
+                st.pyplot(fig)
+            
+                # Plotting feature importances
+                imp, ax = plt.subplots(figsize=(10, 6))
+                ax.bar(range(len(importances)), importances[sorted_indices], align='center')
+                ax.set_xticks(range(len(importances)))
+                ax.set_xticklabels(np.array(features)[sorted_indices])
+                ax.set_title("Feature Importances")
+                ax.set_ylabel('Importance')
+                ax.set_xlabel('Features')
+
+                st.pyplot(imp)  # Pass the figure object to st.pyplot()
+##convert to anohter
+
+if jumlah_beban == "4" :
+        st.image('https://drive.google.com/uc?id=14WooQDOBkHLHqVkCy6wFwLfzb6Jxq8YS')
+        st.write("""##### please make sure if every weight at one side, at least it inclined 1 degree""")
+        bebanA = st.number_input("Weight 1 (Kg)",min_value= 0.0000,  step =0.0001)
+        bebanB = st.number_input("Weight 2 (Kg)",min_value= 0.0000,  step =0.0001)
+        bebanC = st.number_input("Weight 3 (Kg)",min_value= 0.0000,  step =0.0001)
+        bebanD = st.number_input("Weight 4 (Kg)",min_value= 0.0000,  step =0.0001)
+        bebanE = 0
+        bebanF = 0
+        
+
+if jumlah_beban == "6" :
+        st.image('https://drive.google.com/uc?id=1BqM-jtRUqNR5w9NNU2teF4R5qYJ2GI7D')
+        st.write("""##### please make sure if every weight at one side, at least it inclined 1 degree""")
+        bebanA = st.number_input("Weight 1 (Kg)",min_value= 0.0000,  step =0.0001)
+        bebanB = st.number_input("Weight 2 (Kg)",min_value= 0.0000,  step =0.0001)
+        bebanC = st.number_input("Weight 3 (Kg)",min_value= 0.0000,  step =0.0001)
+        bebanD = st.number_input("Weight 4 (Kg)",min_value= 0.0000,  step =0.0001)
+        bebanE = st.number_input("Weight 5 (Kg)",min_value= 0.0000, step =0.0001)
+        bebanF = st.number_input("Weight 6 (Kg)",min_value= 0.0000, step =0.0001)
+        
+        
+
+ok = st.button("Calculate Incline")       
+if ok:
+    st.session_state.button_pressed = True
+
+    #start machine learning process
+    # chnge some data into numeric
+
+    # Split the dataset into training and test sets
     train_data, test_data = train_test_split(data, test_size=0.25, random_state=700)
 
     # Select the features and target variable
