d9d0a0244407c1f707b06a08e4346b4185cc5968
Adanene <128061161+Adanene@users.noreply.github.com>
2023-10-21 12:57:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -109,9 +109,9 @@ if ok:
         'learning_rate': [0.125],
         'subsample': [1.0],
         'colsample_bytree': [1.0],   
-        'reg_alpha': [0.75],  # Using reg_alpha instead of alpha
-        'reg_lambda': [0.75],  # Using reg_lambda instead of lambda
-        'reg_gamma': [0.75]
+        'reg_alpha': [1.0],  # Using reg_alpha instead of alpha
+        'reg_lambda': [1.0],  # Using reg_lambda instead of lambda
+        'reg_gamma': [1.0]
     }
 
     # Create the XGBoost regressor
