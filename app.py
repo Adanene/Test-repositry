70ca4d01a94b36bab3f23bafccb0f6333f3c7127
Adanene <128061161+Adanene@users.noreply.github.com>
2023-10-23 10:36:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ if ok:
     # chnge some data into numeric
 
     # Split the dataset into training and test sets
-    train_data, test_data = train_test_split(data, test_size=0.3, random_state=240)
+    train_data, test_data = train_test_split(data, test_size=0.3, random_state=480)
 
      # Further split the training data to get a validation set for early stopping
     train_data, val_data = train_test_split(train_data, test_size=0.1, random_state=42)
