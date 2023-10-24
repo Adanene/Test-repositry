3e0bd85eec774093dcd0ef0469ae9ceb12a08a01
Adanene <128061161+Adanene@users.noreply.github.com>
2023-10-24 14:21:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ if ok:
         'learning_rate': [0.125],
         'subsample': [1.0],
         'colsample_bytree': [1.0],   
-        'reg_alpha': [0.75],  # Using reg_alpha instead of alpha
+        'reg_alpha': [1.0],  # Using reg_alpha instead of alpha
         'reg_lambda': [0.75],  # Using reg_lambda instead of lambda
         'reg_gamma': [0.75]
     }
