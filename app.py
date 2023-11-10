da160cc18f578a7e27e71ba2df7fdc8d090dc42b
Adanene <128061161+Adanene@users.noreply.github.com>
2023-11-10 14:43:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ if ok:
     
     # Define the parameter grid
     param_grid = {
-        'n_estimators': [500], 
+        'n_estimators': [250], 
         'max_depth': [9],
         'learning_rate': [0.01],
         'subsample': [1.0],
