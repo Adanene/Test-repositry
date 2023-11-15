9df20e5c8c42e06fadd794e31e7dccc70b76cd86
Adanene <128061161+Adanene@users.noreply.github.com>
2023-11-15 13:53:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ if ok:
     param_grid = {
         'n_estimators': [350], 
         'max_depth': [11],
-        'learning_rate': [0.15],
+        'learning_rate': [0.125],
         'subsample': [1.0],
         'colsample_bytree': [1.0],   
         'reg_alpha': [1],  # Using reg_alpha instead of alpha
