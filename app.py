b978a922ec4d442552d052d14295d9229bcc8dc1
Adanene <128061161+Adanene@users.noreply.github.com>
2023-11-20 10:23:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -190,7 +190,7 @@ if st.session_state.button_pressed:
                 st.success("Predictions saved to predictions.csv")
                 
                 def create_download_link(dg, filename="predictions.csv"):
-                    csv = dg.to_csv(index=False, quoting=csv.QUOTE_NONNUMERIC, sep=';')
+                    csv = dg.to_csv(index=False,sep='|')
                     b64 = base64.b64encode(csv.encode()).decode()  # Encoding the CSV file
                     href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">Download CSV</a>'
                     return href
