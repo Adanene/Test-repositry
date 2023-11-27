1ab2c36abe44c37ae2a6ed9851c4991a8c91c9bc
Adanene <128061161+Adanene@users.noreply.github.com>
2023-11-27 10:38:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ if ok:
                            cv=3, n_jobs=-1, verbose=2, scoring='neg_mean_squared_error', error_score='raise')
 
     # Fit the GridSearchCV to the training data
-    grid_search.fit(X, y, eval_metric='rmse', eval_set=[(X, y)], early_stopping_rounds=1547)
+    grid_search.fit(X, y, eval_metric='rmse', eval_set=[(X, y)], early_stopping_rounds=600)
 
     # Get the best model from GridSearchCV
     best_model = grid_search.best_estimator_
@@ -164,7 +164,7 @@ if ok:
     # Now, all_predictions_best_model and all_predictions_model contain the predictions for all data points in your dataset
 
     # Apply the threshold to predicted values
-    threshold = 0.05  # You can adjust this value based on your domain knowledge
+    threshold = 0.001  # You can adjust this value based on your domain knowledge
     all_predictions_best_model[all_predictions_best_model < threshold] = 0
     all_predictions_model[all_predictions_model < threshold] = 0
 
