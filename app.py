f90744ad548e2512a6a2fd7ea448a6640b174bc1
Adanene <128061161+Adanene@users.noreply.github.com>
2023-12-08 13:47:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ if ok:
         'reg_alpha': [1],
         'reg_lambda': [1],
         'reg_gamma': [1],
-        'min_child_weight': [3] ,
+        'min_child_weight': [4] ,
     }
 
     # Create the XGBoost regressor
