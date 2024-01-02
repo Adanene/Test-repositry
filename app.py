1eccf3fc81f18f7a0ea999d92c54ecc5e1dd399d
Adanene <128061161+Adanene@users.noreply.github.com>
2024-01-02 18:09:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -642,17 +642,17 @@ if st.session_state.button_pressed:
 
                  # Build the table
                 
-                KG: [KG1, KG2, KG3, KG4, KG5, KG6, KG7, KG8, KG9]
-                MG: [MG1, MG2, MG3, MG4, MG5, MG6, MG7, MG8, MG9]
-                KM: [KM1, KM2, KM3, KM4, KM5, KM6, KM7, KM8, KM9]
-                Cogm: [Cogm1, Cogm2, Cogm3, Cogm4, Cogm5, Cogm6, Cogm7, Cogm8, Cogm9]
+                KGdata : [KG1, KG2, KG3, KG4, KG5, KG6, KG7, KG8, KG9]
+                MGdata : [MG1, MG2, MG3, MG4, MG5, MG6, MG7, MG8, MG9]
+                KMdata : [KM1, KM2, KM3, KM4, KM5, KM6, KM7, KM8, KM9]
+                Cogmdata : [Cogm1, Cogm2, Cogm3, Cogm4, Cogm5, Cogm6, Cogm7, Cogm8, Cogm9]
                 # Assuming KG, MG, and KM are your original data
 
                 # Round each value to 0.001 depth
-                rounded_KG = [round(value, 3) for value in KG]
-                rounded_MG = [round(value, 3) for value in MG]
-                rounded_KM = [round(value, 3) for value in KM]
-                rounded_Cogm = [round(value, 3) for value in Cogm]
+                rounded_KG = [round(value, 3) for value in KGdata]
+                rounded_MG = [round(value, 3) for value in MGdata]
+                rounded_KM = [round(value, 3) for value in KMdata]
+                rounded_Cogm = [round(value, 3) for value in Cogmdata]
                 # Create the DataFrame
                 dataK = pd.DataFrame({
                     'Cogm' : rounded_Cogm,
