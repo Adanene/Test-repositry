34484ae5170f989ebe1032d00d101b11a7eb8c55
Adanene <128061161+Adanene@users.noreply.github.com>
2024-01-13 11:25:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ if ok:
             xgboost_model = xgb.XGBRegressor(random_state=400, objective="reg:squarederror")
             param_grid = {
                 'n_estimators': [100],
-                'max_depth': [10],
+                'max_depth': [9],
                 'learning_rate': [1.25],
                 'subsample': [ 1],
                 'colsample_bytree': [1.0],
